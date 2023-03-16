53c6132b87edfeca0e25235c31b5568267798b84
7GrandDadVape <127914282+7GrandDadLyno@users.noreply.github.com>
2023-03-16 11:00:19 +0800

    Create Brantlol.lua

---
+++ MainSettings/FUNCTIONS/Working/UnPatched/Main/PrivateScript/Brantlol.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadLyno/LynoV4/main/MainSettings/FUNCTIONS/Working/UnPatched/Main/Private/LynoV4/ForRoblox/eznvmLynoV4.lua" , false))() 
+notify("Script Loadinffg")
