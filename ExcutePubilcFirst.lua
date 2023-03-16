d53f549f0d35c6b76acc0801b80516f7a27e4119
7GrandDadVape <127914282+7GrandDadLyno@users.noreply.github.com>
2023-03-16 11:08:20 +0800

    Create ExcutePubilcFirst.lua

---
+++ ExcutePubilcFirst.lua
@@ -0,0 +1,7 @@
+local CustomConfig = LIB("Render", {
+    Name = "Auto Win | Beta !",
+    Function = function(callback) 
+        if callback then
+		notify("Script For Beta")
+        end
+    end,
